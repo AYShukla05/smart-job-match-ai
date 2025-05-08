b10c4acb9fffe383fef6bff19c45b5958dc206d8
Ansh Shukla <ayshukla2001@gmail.com>
2025-05-08 15:11:52 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Entry point for the Smart Job Match CLI tool
+
+def main():
+    print('Welcome to Smart Job Match AI')
+    print("Welcome to Ansh & Himanshu AI Project")
+
+if __name__ == '__main__':
+    main()
